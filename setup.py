a28209d3c6cf0de3f8b87d6c7e6e4d69c8992570
Naresh <nareshkadambini@gmail.com>
2022-01-10 22:28:09 -0500

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="machine-learning",
+    version="0.0.3",
+    description="machine-learning",
+    author="Naresh Dangol",
+    packages=find_packages(),
+    license="MIT"
+)
